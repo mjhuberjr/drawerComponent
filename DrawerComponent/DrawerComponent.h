4657bfc0182ee9accdfadfcce133932571bb69f9
Michael J. Huber Jr <info@op131.com>
2018-06-11 20:19:33 -0600

    Initial commit

---
+++ DrawerComponent/DrawerComponent.h
@@ -0,0 +1,19 @@
+//
+//  DrawerComponent.h
+//  DrawerComponent
+//
+//  Created by Michael J. Huber Jr. on 6/9/18.
+//  Copyright © 2018 operation thirteenOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawerComponent.
+FOUNDATION_EXPORT double DrawerComponentVersionNumber;
+
+//! Project version string for DrawerComponent.
+FOUNDATION_EXPORT const unsigned char DrawerComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawerComponent/PublicHeader.h>
+
+
