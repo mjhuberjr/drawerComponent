2b709d498a0b6c7e2dc1222eb8ea8d56079a0b4c
Michael J. Huber Jr <info@op131.com>
2018-06-15 08:57:14 -0600

    Quick passthrough test before cleanup

---
+++ DrawerComponent/Sources/Extensions/UIView+Passthrough.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+Passthrough.swift
+//  DrawerComponent
+//
+//  Created by Michael J. Huber Jr. on 6/15/18.
+//  Copyright © 2018 operation thirteenOne. All rights reserved.
+//
+
+import Foundation
+
+class PassthroughView: UIView {
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = super.hitTest(point, with: event)
+        return hitView == self ? nil : hitView
+    }
+}
