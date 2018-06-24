9ebecd568808e85f416e92663fe3d1ba160536b3
Michael J. Huber Jr <info@op131.com>
2018-06-24 17:29:49 -0600

    Allow passthrough to view underneath to allow touch to views underneath

--- DrawerComponent/Sources/Root/DrawerRootViewController.swift
+++ DrawerComponent/Sources/Root/DrawerRootViewController.swift
@@ -19,7 +19,7 @@ public extension DrawerAnimatable where Self: UIViewController { }
 
 public class DrawerRootViewController: UIViewController {
     
-    @IBOutlet var drawerView: UIView!
+    @IBOutlet var drawerView: PassthroughView!
     @IBOutlet public var bottomConstraint: NSLayoutConstraint!
     
     private var presenter: DrawerComponentPresentation
